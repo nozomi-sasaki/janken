5d73c51777c068cd3e9bee687836c29288d84c56
nozomi sasaki <nozomi.sasaki@diva.co.jp>
2019-09-11 15:11:07 +0900

    テストコード追加

--- Result.cs
+++ Result.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,12 +14,21 @@ namespace ConsoleApp1
             //結果出力
             int[] winnum = new int[sumnum];
 
+            Encoding enc = Encoding.GetEncoding("Shift_JIS");
+            string filepath = Path.Combine(@"C:\Users\nozomi.sasaki\source\repos\UnitTestJanken\bin\Debug\Output.txt");
+
+
             for (int i = 0; i < sumnum; i++)
             {
                 winnum[i] = winperson.Count(value => value == i);
                 double shouritsu = 1.0 * winnum[i] / time;
 
                 Console.WriteLine("プレイヤー{0}の勝ち数は{1}、勝率は{2:f2}", i, winnum[i], shouritsu);
+
+                StreamWriter file = new StreamWriter(filepath, true, enc);
+                file.WriteLine("プレイヤー{0}の勝ち数は{1}、勝率は{2:f2}", i, winnum[i], shouritsu);
+
+                file.Close();
             }
 
             Console.WriteLine("\n");
